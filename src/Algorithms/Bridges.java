fa2006e1ee6ac5a76b776d28443387c2890fac86
sanchayharjai <sanchayharjai37@gmail.com>
2022-01-30 18:40:37 +0530

    Bridges

--- src/Algorithms/Bridges.java
+++ src/Algorithms/Bridges.java
@@ -12,6 +12,32 @@ public class Bridges {
             this.from = from;
         }
     }
+    /*
+    a bridge, isthmus, cut-edge, or cut arc is an edge of a graph whose deletion increases the
+    graph's number of connected components. Equivalently, an edge is a bridge if and only if
+     it is not contained in any cycle.
+    First thought
+    An edge is a bridge, if and only if it is not in a cycle.
+    So, if we know how to find cycles, and discard all edges in the cycles, then
+     the remaining connections are a complete collection of bridges.
+    How to find eges in cycles, and remove them -
+    We use DFS algorithm to find cycles and decide whether or not an edge is in a cycle.
+    Define rank of a node: The depth of a node during a DFS. The starting node has a rank 0.
+    Only the nodes on the current DFS path have non-special ranks. In other words, only the
+    nodes that we've started visiting, but haven't finished visiting, have ranks. So 0 <= rank < n.
+    How can "rank" help us with removing cycles? Imagine you have a current path of length k during
+    a DFS. The nodes on the path has increasing ranks from 0 to kand incrementing by 1. Surprisingly,
+    your next visit finds a node that has a rank of p where 0 <= p < k. Why does it happen? Aha!
+    You found a node that is on the current search path! That means, congratulations, you found a cycle!
+
+    But only the current level of search knows it finds a cycle. How does the upper level of search
+    knows, if you backtrack? Let's make use of the return value of DFS: dfs function returns the
+    minimum rank it finds. During a step of search from node u to its neighbor v, if dfs(v) returns
+    something smaller than or equal to rank(u), then u knows its neighbor v helped it to find a cycle
+     back to u or u's ancestor. So u knows it should discard the edge (u, v) which is in a cycle.
+
+    After doing dfs on all nodes, all edges in cycles are discarded. So the remaining edges are bridges.
+     */
     int id = 0;
     public List<Integer> Bridges(List<Edge> graph[]){
         id = 0;
--- src/Algorithms/Prims.java
+++ src/Algorithms/Prims.java
@@ -4,6 +4,18 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Prims {
+    /*
+      Prim's algorithm (also known as Jarník's algorithm) is a greedy algorithm
+      that finds a minimum spanning tree for a weighted undirected graph. This
+      means it finds a subset of the edges that forms a tree that includes every
+      vertex, where the total weight of all the edges in the tree is minimized.
+      The algorithm operates by building this tree one vertex at a time, from an
+      arbitrary starting vertex, at each step adding the cheapest possible connection
+      from the tree to another vertex.Prim’s algorithm assumes that all vertices are
+      connected. But in a directed graph, every node is not reachable from every
+      other node. So, Prim’s algorithm fails due to this reason.
+      Time complexity - O((V+E)logV)
+    */
     public static class Edge {
         int from, to, cost;
 
@@ -19,6 +31,7 @@ public class Prims {
         for(Edge e : graph[cur]) if(!visited[e.to]) pq.add(e);
     }
     public int mstCost(List<Edge> graph[]){
+        // To build path
         edges = new Edge[graph.length-1];
         PriorityQueue<Edge> pq = new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
         boolean visited[] = new boolean[graph.length];
@@ -27,7 +40,9 @@ public class Prims {
         int cost = 0;
         int count = 0;
         while(!pq.isEmpty() && count < graph.length){
+            // Greedily process the cheapest edge available
             Edge cur = pq.poll();
+            // Stale edges
             if(visited[cur.to]) continue;
             cost+=cur.cost;
             edges[count++] = cur;
